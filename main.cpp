f43ab37c09cbec4e0f041b18c2f486bb1139d0b9
Danil <switnosok@gmail.com>
2026-04-05 17:08:37 +0500

    создан classname и isA

--- main.cpp
+++ main.cpp
@@ -8,28 +8,37 @@ public:
     BaseClass()
     {
         std::cout << "Конструктор предка\n";
-    };
+    }
     virtual ~BaseClass()
     {
         std::cout << "Деструктор предка\n";
-    };
+    }
     void method1()
     {
         std::cout << "VМетод1 предка\nВызываем метод2\n";
         method2();
-    };
+    }
     void method2()
     {
         std::cout << "Метод2 предка\n";
-    };
+    }
     void method3()
     {
         std::cout << "Метод3 предка\n";
-    };
+        method4();
+    }
     virtual void method4()
     {
         std::cout << "Метод4 предка\n";
-    };
+    }
+    virtual std::string classname()
+    {
+        return "BaseClass";
+    }
+    virtual bool isA(std::string name)
+    {
+        return name == "BaseClass";
+    }
 };
 
 
@@ -39,11 +48,11 @@ public:
     HeirClass()
     {
         std::cout << "Конструктор потомка\n";
-    };
+    }
     ~HeirClass() override 
     {
         std::cout << "Деструктор потомка\n";
-    };
+    }
     /*
     void method1()
     {
@@ -53,7 +62,7 @@ public:
     void method2()
     {
          std::cout << "Метод2 потомка\n";
-    };
+    }
     /*
     void method3()
     {
@@ -63,7 +72,15 @@ public:
     void method4() override
     {
          std::cout << "Метод4 потомка\n";
-    };
+    }
+    bool isA(std::string name) override
+    {
+        return name == "HeirClass" || BaseClass::isA(name);
+    }
+    std::string classname() override
+    {
+        return "HeirClass";
+    }
 };
 
 void foo1()
@@ -158,7 +175,54 @@ void foo4()
     std::cout << "============================================================\n";
 }
 
+void foo5()
+{
+    std::cout << "============================================================\n";
+    std::cout << "Создание обьектов диномически BaseClass = new BaseClass\n";
+    BaseClass* BC1 = new BaseClass;
+    std::cout << "Создание обьектов диномически BaseClass = new HeirClass\n";
+    BaseClass* BC2 = new HeirClass;
+    std::cout << "Создание обьектов диномически HeirClass = new HeirClass\n";
+    HeirClass* HC = new HeirClass; 
 
+    std::cout << "Вызов classname у BaseClass = new BaseClass\n";
+    BC1->classname();
+
+    std::cout << "Вызов classname у BaseClass = new HeirClass\n";
+    BC2->classname();
+
+    std::cout << "Вызов classname у HeirClass = new HeirClass:\n";
+    HC->classname();
+
+
+
+    std::cout << "Проверка isA(BaseClass) у BaseClass = new BaseClass:\n";
+    BC1->isA("BaseClass");
+
+    std::cout << "Проверка isA(BaseClass) у BaseClass = new HeirClass:\n";
+    BC2->isA("BaseClass");
+
+    std::cout << "Проверка isA(BaseClass) у HeirClass = new HeirClass:\n";
+    HC->isA("BaseClass");
+
+    
+    std::cout << "Проверка isA(HeirClass) у BaseClass = new BaseClass:\n";
+    BC1->isA("BaseClass");
+
+    std::cout << "Проверка isA(HeirClass) у BaseClass = new HeirClass:\n";
+    BC2->isA("BaseClass");
+
+    std::cout << "Проверка isA(HeirClass) у HeirClass = new HeirClass:\n";
+    HC->isA("BaseClass");
+
+    std::cout << "Деструктор BaseClass = new BaseClass\n";
+    delete BC1;
+    std::cout << "Деструктор BaseClass = new HeirClass\n";
+    delete BC2;
+    std::cout << "Деструктор HeirClass = new HeirClass:\n";
+    delete HC;
+    std::cout << "============================================================\n";
+}
 int main() {
     SetConsoleOutputCP(CP_UTF8);                                             
     SetConsoleCP(CP_UTF8);
@@ -169,5 +233,7 @@ int main() {
     std::cout << "Метод3 у родителя и вызывает переопределённый Метод2 виртуальный\n";
     foo3();
     foo4();
+    std::cout << "Вызов classname и isA\n";
+    foo5();
     return 0;
 }
